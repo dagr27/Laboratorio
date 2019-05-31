ba9e0f69dd0915145570c30a6e7befad094b0f8a
dagr27 <00118717@uca.edu.sv>
2019-05-31 14:46:49 -0600

    Subiendo labo

---
+++ app/src/main/java/com/petrlr14/mvvm/database/repositories/GitHubRepoRepository.kt
@@ -0,0 +1,31 @@
+package com.petrlr14.mvvm.database.repositories
+
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
+import com.petrlr14.mvvm.database.daos.GitHubDAO
+import com.petrlr14.mvvm.database.entities.GitHubRepo
+import com.petrlr14.mvvm.service.retrofit.GithubService
+import kotlinx.coroutines.Deferred
+import retrofit2.Response
+
+class GitHubRepoRepository (private val repoDao:GitHubDAO){
+
+    fun retrieveRepoAsync(user:String):Deferred<Response<List<GitHubRepo>>>{
+        return GithubService.getRetrofit().getRepos(user)
+    }
+
+    @WorkerThread
+    suspend fun insert(repo:GitHubRepo){
+        repoDao.insert(repo)
+    }
+
+    fun getAll():LiveData<List<GitHubRepo>>{
+        return repoDao.getAllRepos()
+    }
+
+    @WorkerThread
+    suspend fun nuke(){
+        return repoDao.nukeTable()
+    }
+
+}
